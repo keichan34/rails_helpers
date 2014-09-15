a1e586a3598282a47d170b654d17d2e3e5007a5b
Keitaroh Kobayashi <keita@kkob.us>
2014-09-15 10:00:00 +0900

    Apppend classes to body_classes

--- lib/rails_helpers/helpers/body_classes.rb
+++ lib/rails_helpers/helpers/body_classes.rb
@@ -2,12 +2,12 @@ module RailsHelpers::Helpers
   module BodyClasses
     extend ActiveSupport::Concern
 
-    def body_classes
+    def body_classes(other_classes=[])
       controller_names = controller.controller_path.split '/'
       controller_name = controller_names.join '__'
       controller_names.pop
 
-      controller_names + [controller_name, "#{controller_name}-#{controller.action_name}"]
+      other_classes + controller_names + [controller_name, "#{controller_name}-#{controller.action_name}"]
     end
   end
 end
